0f4d78e8886c8488afb22bd05f82154ed13d0641
fabionogueira <fabio.bacabal@gmail.com>
2018-02-06 19:05:02 -0200

    ...

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'dist/index.js'
+    },
+    module:{
+        rules: [
+            {
+               use: {
+                  loader:'babel-loader',
+                  options: { presets: ['es2015'] }
+               },
+               test: /\.js$/,
+               exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJsPlugin()
+    ]
+};
